c1a65cd14e68f20065aed04134520b77217b6d66
Zuldajri <w.matyas15@hotmail.fr>
2021-10-07 18:42:44 +0200

    Update setup-nfs.sh

--- scripts/setup-nfs.sh
+++ scripts/setup-nfs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
-#SSL certificate issue fix: https://access.redhat.com/solutions/3167021
-yum update --disablerepo=* --enablerepo="*microsoft*"
+yum update -y --disablerepo=* --enablerepo="*microsoft*"
 yum install -y nfs-utils
 yum install -y rpcbind
 systemctl unmask firewalld
@@ -10,10 +8,9 @@ systemctl start nfs-server
 systemctl enable nfs-server
 mkdir -p /exports/home
 echo "/exports/home *(rw,sync,subtree_check,no_root_squash)" >> /etc/exports
-drive=$(fdisk -l | awk '$1 == "Disk" && $2 ~ /^\// && ! /mapper/ {if (drive) print drive; drive = $2; sub(":", "", drive);} drive && /^\// {drive = ""} END {if (drive) print drive;}')
-mkfs.xfs $drive
+mkfs.xfs /dev/sdc
 sleep 10
-mount $drive /exports/home
+mount /dev/sdc /exports/home
 chown -R nfsnobody:nfsnobody /exports/home
 chmod -R 777 /exports/home
 exportfs -a
@@ -21,4 +18,4 @@ firewall-cmd --permanent --add-service=mountd
 firewall-cmd --permanent --add-service=nfs
 firewall-cmd --permanent --add-service=rpc-bind
 firewall-cmd --reload
-echo "$(sudo blkid | grep $drive | awk '{print $2}') /exports/home        xfs     defaults    0 0" >> /etc/fstab
+echo "$(sudo blkid | grep /dev/sdc | awk '{print $2}') /exports/home        xfs     defaults    0 0" >> /etc/fstab
